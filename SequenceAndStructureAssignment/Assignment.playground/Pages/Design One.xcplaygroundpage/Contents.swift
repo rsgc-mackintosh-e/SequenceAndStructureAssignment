fab6ac4e0ab7ed95529f4127b15a840c44dd27d7
Euan Mackintosh <student@macbook.local>
2017-03-03 10:29:52 -0500

    Drew a bunch of circles

--- SequenceAndStructureAssignment/Assignment.playground/Pages/Design One.xcplaygroundpage/Contents.swift
+++ SequenceAndStructureAssignment/Assignment.playground/Pages/Design One.xcplaygroundpage/Contents.swift
@@ -21,25 +21,65 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 500, height: 500)
 
-// Generate a grid
+//// Generate a grid
 canvas.drawShapesWithFill = false
 canvas.defaultBorderWidth = 1
+//
+//
+//// This loop makes a 10 rows of columns
+//for x in stride(from: 25, through: 475, by: 50){
+//    
+//    // This loop makes a single column, bottom to top
+//    for y in stride(from: 25, through: 475, by: 50) {
+//        
+//        // Draw the shapes
+//        canvas.drawEllipse(centreX: x, centreY: y, width: 2, height: 2)
+//        canvas.drawRectangle(centreX: x, centreY: y, width: 50, height: 50)
+//    }
+//}
+
+//canvas.fillColor
+//canvas.drawRectangle(bottomLeftX: 50, bottomLeftY: 50, width: 50, height: 50)
+//canvas.drawRectangle(bottomLeftX: 100, bottomLeftY: 100, width: 50, height: 50)
+//
 
 
-// This loop makes a 10 rows of columns
-for x in stride(from: 25, through: 475, by: 50){
-    
-    // This loop makes a single column, bottom to top
-    for y in stride(from: 25, through: 475, by: 50) {
-        
-        // Draw the shapes
-        canvas.drawEllipse(centreX: x, centreY: y, width: 2, height: 2)
-        canvas.drawRectangle(centreX: x, centreY: y, width: 50, height: 50)
-    }
+//canvas.borderColor = Color.red
+//
+//for x in stride(from: 0, through: 500, by: 20){
+//    canvas.drawEllipse(centreX: 250, centreY: 250, width: x, height: x)
+//}
+
+canvas.borderColor = Color.red
+
+for x in stride(from: 0, through: 500, by: 20){
+    canvas.drawEllipse(centreX: 0, centreY: 0, width: x, height: x)
 }
 
 
+canvas.borderColor = Color.blue
 
+for x in stride(from: 0, through: 500, by: 20){
+    canvas.drawEllipse(centreX: 500, centreY: 0, width: x, height: x)
+}
+
+canvas.borderColor = Color.green
+
+
+for x in stride(from: 0, through: 500, by: 20){
+    canvas.drawEllipse(centreX: 500, centreY: 500, width: x, height: x)
+}
+
+canvas.borderColor = Color.purple
+
+
+for x in stride(from: 0, through: 500, by: 20){
+    canvas.drawEllipse(centreX: 0, centreY: 500, width: x, height: x)
+}
+
+//for x in stride(from: 250, through: 0, by: -25){
+//canvas.drawLine(fromX: 250, fromY: 250, toX: x, toY: x)
+//}
 
 /*:
  ## Template code
