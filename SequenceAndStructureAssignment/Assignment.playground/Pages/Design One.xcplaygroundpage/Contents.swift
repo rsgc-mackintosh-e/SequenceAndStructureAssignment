e6bb3687e4eb04a10c26058ba65e4f22ffd23f0b
Euan Mackintosh <student@macbook.local>
2017-03-09 09:53:48 -0500

    I think I'm dun

--- SequenceAndStructureAssignment/Assignment.playground/Pages/Design One.xcplaygroundpage/Contents.swift
+++ SequenceAndStructureAssignment/Assignment.playground/Pages/Design One.xcplaygroundpage/Contents.swift
@@ -21,53 +21,63 @@ import PlaygroundSupport
 // Create canvas
 let canvas = Canvas(width: 500, height: 500)
 
-//// Generate a grid
+// Generate a grid
 canvas.drawShapesWithFill = true
-canvas.defaultBorderWidth = 1
+canvas.defaultBorderWidth = 5
+canvas.defaultLineWidth = 3
 
 canvas.fillColor = Color.black
 canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 500, height: 500)
 
-
+//Create the lines that are in the middle
 for x in stride(from: 0, through: 500, by: 500){
-    for y in stride(from: 0, through: 500, by: 20){
+    for y in stride(from: 0, through: 500, by: 50){
         canvas.lineColor = Color(hue: y, saturation: 100, brightness: 100, alpha: 100)
         canvas.drawLine(fromX: 250, fromY: 250, toX: x, toY: y)
     }
 }
 
 for x in stride(from: 0, through: 500, by: 500){
-    for y in stride(from: 0, through: 500, by: 20){
+    for y in stride(from: 0, through: 500, by: 30){
         canvas.lineColor = Color(hue: y, saturation: 100, brightness: 100, alpha: 100)
         canvas.drawLine(fromX: 250, fromY: 250, toX: y, toY: x)
     }
 }
 
-
+//Set the background color of the circles
 canvas.fillColor = Color.black
 
+//Generate a random number from 0 to 360
 var c = random(from: 0, toButNotIncluding: 361)
 
+//Use c as the value for hue to chance the color
 canvas.borderColor = Color.init(hue: c, saturation: 100, brightness: 100, alpha: 100)
-for x in stride(from: 500, through: 0, by: -10){
+
+//draw the bottom left set of circles
+for x in stride(from: 500, through: 0, by: -20){
     canvas.drawEllipse(centreX: 0, centreY: 0, width: x, height: x)
 }
 
+//Get the remainder of dividing it by 360 to get the complimentary color
 var sc = (c + 180) % 360
 canvas.borderColor = Color.init(hue: sc, saturation: 100, brightness: 100, alpha: 100)
-for x in stride(from: 500, through: 0, by: -10){
+
+//Draw the bottom right circles (using loops)
+for x in stride(from: 500, through: 0, by: -20){
     canvas.drawEllipse(centreX: 500, centreY: 0, width: x, height: x)
 }
 
 canvas.borderColor = Color.init(hue: c, saturation: 100, brightness: 100, alpha: 100)
 
-for x in stride(from: 500, through: 0, by: -10){
+//Draw the top right set of circles
+for x in stride(from: 500, through: 0, by: -20){
     canvas.drawEllipse(centreX: 500, centreY: 500, width: x, height: x)
 }
 
 canvas.borderColor = Color.init(hue: sc, saturation: 100, brightness: 100, alpha: 100)
 
-for x in stride(from: 500, through: 0, by: -10){
+//Draw the top left set of circles
+for x in stride(from: 500, through: 0, by: -20){
     canvas.drawEllipse(centreX: 0, centreY: 500, width: x, height: x)
 }
 
